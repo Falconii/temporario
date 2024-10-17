1e6639fc6fefc2ea56749173caee1d5ccf14441b
Marcos Renato Falconi <falconi.2050@gmail.com>
2024-10-17 15:36:54 -0300

    amem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'temporario';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log("Cheguei Aqui !!!");
+  }
 }
+
